a8ea948be7661eb769c9784e5171cebd5ecd5461
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-12-26 21:48:16 +0100

    add input labels

--- book-library/src/components/SearchBar.jsx
+++ book-library/src/components/SearchBar.jsx
@@ -18,6 +18,8 @@ const SearchBar = () => {
 
   return (
     <form onSubmit={handleSearch} className="mb-4 flex flex-col sm:flex-row gap-4 sm:gap-2 justify-center">
+      <div className="flex flex-col">
+      <label htmlFor="" className="p-2">Search By Title</label>
       <input
         type="text"
         value={title}
@@ -25,6 +27,9 @@ const SearchBar = () => {
         placeholder="Search by book title ..."
         className="border px-4 py-2 w-full sm:w-64"
       />
+      </div>
+    <div className="flex flex-col">
+      <label htmlFor="" className="p-2">Search By Author</label>
       <input
         type="text"
         value={author}
@@ -32,6 +37,7 @@ const SearchBar = () => {
         placeholder="Search by author..."
         className="border px-4 py-2 w-full sm:w-64"
       />
+    </div>
       <button type="submit" className="bg-blue-500 text-white px-6 py-2 rounded mt-2 sm:mt-0 sm:w-auto">
         Search
       </button>
